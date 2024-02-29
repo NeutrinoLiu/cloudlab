13ec29220745460b58a4b1612088c4cc7db4dccf
NeutrinoLiu <liubangya@gmail.com>
2024-02-28 22:35:55 -0600

    Create install_colmap.sh

---
+++ 3dgs/install_colmap.sh
@@ -0,0 +1,34 @@
+# install COLMAP
+
+sudo apt-get install \
+    git \
+    cmake \
+    ninja-build \
+    build-essential \
+    libboost-program-options-dev \
+    libboost-filesystem-dev \
+    libboost-graph-dev \
+    libboost-system-dev \
+    libeigen3-dev \
+    libflann-dev \
+    libfreeimage-dev \
+    libmetis-dev \
+    libgoogle-glog-dev \
+    libgtest-dev \
+    libsqlite3-dev \
+    libglew-dev \
+    qtbase5-dev \
+    libqt5opengl5-dev \
+    libcgal-dev \
+    libceres-dev
+
+cd ~/Desktop
+git clone https://github.com/colmap/colmap.git
+cd colmap
+mkdir build
+cd build
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/usr/local/cuda-11.8/lib64
+export PATH=${PATH}:/usr/local/cuda-11.8/bin
+cmake .. -GNinja -DCMAKE_CUDA_ARCHITECTURES=70 #V100 is 70
+ninja
+sudo ninja install
