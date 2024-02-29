2d8cbb5d9eec30df0b782e3cc26849c9c7bfaee6
NeutrinoLiu <liubangya@gmail.com>
2024-02-29 10:43:21 -0600

    Create download_data.sh

---
+++ 3dgs/download_data.sh
@@ -0,0 +1,17 @@
+# download dataset =============================
+
+sudo mkdir /data/dataset
+sudo chmod 777 -R /data/dataset
+ln -s /data/dataset ~/Desktop/dataset
+cd ~/Desktop/dataset
+
+wget https://repo-sam.inria.fr/fungraph/3d-gaussian-splatting/datasets/input/tandt_db.zip
+wget -O dnerf_db.zip https://www.dropbox.com/s/0bf6fl0ye2vz3vr/data.zip
+
+conda activate gaussian_splatting
+conda install gdown
+gdown https://drive.google.com/file/d/18JxhpWD-4ZmuFKLzKlAw-w5PpzZxXOcG/view\?usp\=drive_link --fuzzy
+
+unzip tandt_db.zip
+unzip dnerf_db.zip
+unzip nerf_synthetic.zip
