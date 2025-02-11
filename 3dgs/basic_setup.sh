1aa65dc28fdacf27c3d25fe8dc8e0ab1e7351582
Bangya Liu <liubangya@gmail.com>
2025-02-11 16:31:52 -0600

    Update and rename before_reboot.sh to basic_setup.sh

---
+++ 3dgs/basic_setup.sh
@@ -0,0 +1,21 @@
+# desktop gui =============================
+sudo apt update
+sudo apt upgrade -y
+sudo apt install xfce4 xrdp htop git tmux -y
+
+# install omz
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# install miniconda =============================
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda-installer.sh
+bash ./miniconda-installer.sh -u -b
+rm miniconda-installer.sh
+source ~/miniconda3/etc/profile.d/conda.sh
+conda init zsh
+
+# install cuda11.8 =============================
+sudo bash install_nv_driver.sh
+sudo bash install_cuda_118.sh
+
+# reboot after =============================
+sudo reboot
