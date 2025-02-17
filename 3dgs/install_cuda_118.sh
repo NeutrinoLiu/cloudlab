137349bce02796b9f7c65e4a420ef27b63935619
Bangya Liu <liubangya@gmail.com>
2025-02-16 22:38:28 -0600

    Update install_cuda_118.sh

--- 3dgs/install_cuda_118.sh
+++ 3dgs/install_cuda_118.sh
@@ -1,5 +1,5 @@
 wget https://developer.download.nvidia.com/compute/cuda/11.8.0/local_installers/cuda_11.8.0_520.61.05_linux.run
-sudo sh ./cuda_11.8.0_520.61.05_linux.run --toolkit --samples --silent --override
+sh ./cuda_11.8.0_520.61.05_linux.run --toolkit --samples --silent --override
 echo 'export PATH=/usr/local/cuda-11.8/bin${PATH:+:${PATH}}' >> ~/.zshrc
 echo 'export LD_LIBRARY_PATH=/usr/local/cuda-11.8/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}' >> ~/.zshrc
 echo 'export PATH=/usr/local/cuda-11.8/bin${PATH:+:${PATH}}' >> ~/.bashrc
